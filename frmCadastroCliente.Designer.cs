800f6f5b4d6c5c1dfd9364f3ea0312739bce8cea
Guilherme Rodrigues <147875655+GuilhermeRodriguees@users.noreply.github.com>
2023-12-18 17:58:36 -0300

    feat: estudando-c#

---
+++ frmCadastroCliente.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace Cadastro
+{
+    partial class frmCadastroCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCadastroCliente));
+            this.btnClose = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCadastro = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.txtSenha2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.lblSenha2 = new System.Windows.Forms.Label();
+            this.lblSenha3 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.AutoSize = true;
+            this.btnClose.BackColor = System.Drawing.Color.Transparent;
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnClose.Location = new System.Drawing.Point(770, 9);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(18, 19);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "X";
+            this.btnClose.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(66, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(302, 56);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cadastro de";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(66, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(211, 56);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Clientes";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label4.Location = new System.Drawing.Point(81, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Estudando C#";
+            // 
+            // btnCadastro
+            // 
+            this.btnCadastro.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(58)))), ((int)(((byte)(237)))));
+            this.btnCadastro.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCadastro.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCadastro.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastro.ForeColor = System.Drawing.Color.White;
+            this.btnCadastro.Location = new System.Drawing.Point(569, 354);
+            this.btnCadastro.Name = "btnCadastro";
+            this.btnCadastro.Size = new System.Drawing.Size(136, 23);
+            this.btnCadastro.TabIndex = 4;
+            this.btnCadastro.Text = "Cadastre-se";
+            this.btnCadastro.UseVisualStyleBackColor = false;
+            this.btnCadastro.Click += new System.EventHandler(this.btnCadastro_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(537, 155);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(208, 20);
+            this.txtNome.TabIndex = 5;
+            this.txtNome.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNome_KeyPress);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(537, 203);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(208, 20);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(537, 250);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(208, 20);
+            this.txtSenha.TabIndex = 7;
+            this.txtSenha.UseSystemPasswordChar = true;
+            // 
+            // txtSenha2
+            // 
+            this.txtSenha2.Location = new System.Drawing.Point(537, 298);
+            this.txtSenha2.Name = "txtSenha2";
+            this.txtSenha2.Size = new System.Drawing.Size(208, 20);
+            this.txtSenha2.TabIndex = 8;
+            this.txtSenha2.UseSystemPasswordChar = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(534, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Nome:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(534, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Email:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(534, 234);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 15);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Senha:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(534, 283);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(87, 15);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Repita a senha:";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.BackColor = System.Drawing.Color.Transparent;
+            this.lblNome.Font = new System.Drawing.Font("Lucida Sans Unicode", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(634, 177);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(0, 13);
+            this.lblNome.TabIndex = 13;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.BackColor = System.Drawing.Color.Transparent;
+            this.lblEmail.Font = new System.Drawing.Font("Lucida Sans Unicode", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(669, 226);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(0, 13);
+            this.lblEmail.TabIndex = 14;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.BackColor = System.Drawing.Color.Transparent;
+            this.lblSenha.Font = new System.Drawing.Font("Lucida Sans Unicode", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.Location = new System.Drawing.Point(575, 273);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(0, 13);
+            this.lblSenha.TabIndex = 15;
+            // 
+            // lblSenha2
+            // 
+            this.lblSenha2.AutoSize = true;
+            this.lblSenha2.BackColor = System.Drawing.Color.Transparent;
+            this.lblSenha2.Font = new System.Drawing.Font("Lucida Sans Unicode", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha2.Location = new System.Drawing.Point(575, 321);
+            this.lblSenha2.Name = "lblSenha2";
+            this.lblSenha2.Size = new System.Drawing.Size(0, 13);
+            this.lblSenha2.TabIndex = 16;
+            // 
+            // lblSenha3
+            // 
+            this.lblSenha3.AutoSize = true;
+            this.lblSenha3.BackColor = System.Drawing.Color.Transparent;
+            this.lblSenha3.Font = new System.Drawing.Font("Lucida Sans Unicode", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha3.Location = new System.Drawing.Point(556, 380);
+            this.lblSenha3.Name = "lblSenha3";
+            this.lblSenha3.Size = new System.Drawing.Size(0, 13);
+            this.lblSenha3.TabIndex = 17;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.BackColor = System.Drawing.Color.Transparent;
+            this.lbl1.ForeColor = System.Drawing.Color.Green;
+            this.lbl1.Location = new System.Drawing.Point(751, 158);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(0, 13);
+            this.lbl1.TabIndex = 18;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.BackColor = System.Drawing.Color.Transparent;
+            this.lbl2.ForeColor = System.Drawing.Color.Green;
+            this.lbl2.Location = new System.Drawing.Point(751, 206);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(0, 13);
+            this.lbl2.TabIndex = 19;
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.BackColor = System.Drawing.Color.Transparent;
+            this.lbl3.ForeColor = System.Drawing.Color.Green;
+            this.lbl3.Location = new System.Drawing.Point(751, 253);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(0, 13);
+            this.lbl3.TabIndex = 20;
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.BackColor = System.Drawing.Color.Transparent;
+            this.lbl4.ForeColor = System.Drawing.Color.Green;
+            this.lbl4.Location = new System.Drawing.Point(751, 301);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(0, 13);
+            this.lbl4.TabIndex = 21;
+            // 
+            // frmCadastroCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.lblSenha3);
+            this.Controls.Add(this.lblSenha2);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSenha2);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btnCadastro);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnClose);
+            this.Name = "frmCadastroCliente";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label btnClose;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCadastro;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.TextBox txtSenha2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Label lblSenha2;
+        private System.Windows.Forms.Label lblSenha3;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+    }
+}
---
+++ frmCadastroCliente.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace Cadastro
+{
+    public partial class frmCadastroCliente : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+       (
+           int nLeftRect,     // x-coordinate of upper-left corner
+           int nTopRect,      // y-coordinate of upper-left corner
+           int nRightRect,    // x-coordinate of lower-right corner
+           int nBottomRect,   // y-coordinate of lower-right corner
+           int nWidthEllipse, // height of ellipse
+           int nHeightEllipse // width of ellipse
+       );
+
+        public frmCadastroCliente()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNome_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar))
+            {
+                //Atribui True no Handled para cancelar o evento
+                e.Handled = true;
+            }
+        }
+
+        private void btnCadastro_Click(object sender, EventArgs e)
+        {
+            var novoArquivo = txtNome.Text + ".txt";
+
+            if(File.Exists(novoArquivo))
+            {
+                MessageBox.Show("O cliente já existe!");
+                return;
+            }
+
+            if(txtNome.Text.Contains(" ") && txtNome.Text.Length > 8)
+            {
+                lblNome.Text = "";
+                lbl1.Text = "✅";
+            } 
+            else
+            {
+                lblNome.ForeColor = Color.DarkRed;
+                lblNome.Text = "*Digite seu nome completo";
+                lbl1.Text = "";
+            }
+
+            if(txtEmail.Text.Contains("@gmail.com") || txtEmail.Text.Contains("@outlook.com") || txtEmail.Text.Contains("@hotmail.com"))
+            {
+                lblEmail.Text = "";
+                lbl2.Text = "✅";
+            } 
+            else
+            {
+                lblEmail.ForeColor = Color.DarkRed;
+                lblEmail.Text = "*Digite seu e-mail";
+                lbl2.Text = "";
+            }
+
+            if(txtSenha.Text != txtSenha2.Text && txtSenha.Text.Length < 8 && txtSenha2.Text.Length < 8)
+            {
+                lblSenha3.ForeColor = Color.DarkRed;
+                lblSenha3.Text = "*Verifique se as senhas estão diferentes";
+                lbl3.Text = "";
+                lbl4.Text = "";
+            } else
+            {
+                lblSenha3.Text = "";
+                lbl3.Text = "";
+                lbl4.Text = "";
+            }
+
+            if(txtSenha.Text.Length >= 8 && txtSenha2.Text.Length >= 8 && (txtSenha.Text == txtSenha2.Text))
+            {
+                lblSenha.Text = "";
+                lblSenha2.Text = "";
+                lbl3.Text = "✅";
+                lbl4.Text = "✅";
+            } else
+            {
+                lblSenha.ForeColor = Color.DarkRed;
+                lblSenha2.ForeColor = Color.DarkRed;
+                lblSenha.Text = "*A senha deve conter no mínimo 8 digitos";
+                lblSenha2.Text = "*A senha deve conter no mínimo 8 digitos";
+                lbl3.Text = "";
+                lbl4.Text = "";
+            }
+
+            if(txtNome.Text.Contains(" ") && txtNome.Text.Length > 8 && (txtEmail.Text.Contains("@gmail.com") || txtEmail.Text.Contains("@outlook.com") || txtEmail.Text.Contains("@hotmail.com")) && txtSenha.Text.Length >= 8 && txtSenha2.Text.Length >= 8 && (txtSenha.Text == txtSenha2.Text))
+            {
+                var arquivo = File.CreateText(novoArquivo);
+
+                arquivo.WriteLine(txtNome.Text);
+                arquivo.WriteLine(txtEmail.Text);
+                arquivo.WriteLine(txtSenha.Text);
+                arquivo.Flush();
+                arquivo.Close();
+
+                MessageBox.Show("O cliente foi cadastrado com sucesso!");
+                return;
+            }
+        }
+    }
+}
